01c810043b2e29e065802b903ce8a6e2c118a847
KEVIN_MEDINA_TIJERINA <a.20308051281016@cbtis128.edu.mx>
2023-05-22 22:25:22 +0000

    Hola

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,29 @@
 import 'package:flutter/material.dart';
+import 'screens/secondary.dart';
+import 'screens/primary.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
-    );
+        //Quitamos el debug
+        debugShowCheckedModeBanner: false,
+        //home:BotonFlotante(),
+        //// la pantalla inicial
+        initialRoute: "/primary",
+        routes: {
+          "/primary": (BuildContext context) => Primary(),
+          "/secondary": (BuildContext context) => Secondary(),
+        },
+        theme: ThemeData(
+            primaryColor: Colors.green,
+            accentColor: Colors.orange,
+            //configurar texto
+            textTheme: TextTheme(
+              bodyText2: TextStyle(color: Colors.black, fontSize: 20),
+            )));
   }
 }
